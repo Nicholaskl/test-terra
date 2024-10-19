b4bb20786e1e62c25a0c9bf3a9a427094433c5a4
Nick <49146590+Nicholaskl@users.noreply.github.com>
2024-10-19 14:05:50 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,3 @@ resource "aws_instance" "app_server" {
     Name = var.instance_name
   }
 }
-
-aws_access_key_id = AKIA2UC3BSXMFU72VM5M
-aws_secret_access_key = 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
